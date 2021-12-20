c330ada60b950ab8d5527b6bd2892ceb76a68018
Zeligus <91001472+Zeligus@users.noreply.github.com>
2021-12-19 21:49:38 -0700

    Add files via upload

---
+++ Week_3_Challenge/friendly-parakeet/Develop/script.js
@@ -0,0 +1,40 @@
+// Assignment code here
+var  lower ="abcdefghijklmnopqrstuvwxyz"
+var capitals = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+var  numbers = "0123456789"
+var symbols = "!@#$%^&*()"
+
+
+// Get references to the #generate element
+var generateBtn = document.querySelector("#generate");
+
+
+
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+  for (var i = 0; i <= passwordLength; i++) {
+    var randomNumber = Math.floor(Math.random() * chars.length);
+    password += chars.substring(randomNumber, randomNumber +1);
+   if (capitals = 1) capitals<= passwordLength; i+2;) {
+     else ( generatePassword())
+   }
+   if (var capitals = 1; capitals<= passwordLength; i+2;)
+  }
+
+  passwordText.value = password;
+
+}
+
+generateBtn.addEventListener("click", writePassword); { let password = "";
+var checked = document.getElementById("checkbox").checked;
+var final_string = string;
+console.log(checked);
+if (checked) {
+final_string += "@#$%^&*";
+}
+
+  
+}
+  
